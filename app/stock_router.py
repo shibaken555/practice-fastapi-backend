e7e884206f92ec18a621ab2315cf6948cac3629d
shibaken <ychaishan@gmail.com>
2025-01-24 17:08:41 +0900

    単一の企業情報を返却するAPIは、配列を返却しないように修正

--- app/stock_router.py
+++ app/stock_router.py
@@ -12,21 +12,21 @@ async def fetch_stocks(ticker_symbol):
     input_ticker_symbol = yf.Ticker(ticker_symbol)
     company_info = input_ticker_symbol.info
     if company_info.get("trailingPegRatio") is None:
-        return [
+        return {
             {"ticker_symbol": ticker_symbol},
             {"company_info": "存在しないティッカーシンボルです"},
-        ]
+        }
     else:
         # 企業名、株価、時価総額を取得し返却する
         company_name = company_info.get("longName")
         stock_price = company_info.get("currentPrice")
         market_cap = company_info.get("marketCap")
-        return [
-            {"ticker_symbol": ticker_symbol},
-            {"company_name": company_name},
-            {"stock_price": stock_price},
-            {"market_cap": market_cap},
-        ]
+        return {
+            "ticker_symbol": ticker_symbol,
+            "company_name": company_name,
+            "stock_price": stock_price,
+            "market_cap": market_cap,
+        }
 
 
 # 入力したティッカーシンボルに合致する企業情報を返却するAPI
@@ -35,10 +35,10 @@ async def fetch_stocks(ticker_symbol):
     input_ticker_symbol = yf.Ticker(ticker_symbol)
     company = input_ticker_symbol.info
     if company.get("trailingPegRatio") is None:
-        return [
+        return {
             {"ticker_symbol": ticker_symbol},
             {"company_info": "存在しないティッカーシンボルです"},
-        ]
+        }
     else:
         company_info = company.get("longBusinessSummary")
         return [{"ticker_symbol": ticker_symbol}, {"company_info": company_info}]
